f3cca5c60a2eaafd72f81e313419eede371f2d19
Harrington Joseph <harph@hjoseph.com>
2016-02-03 23:43:05 -0500

    Removed InsufficientNumberOfDocumentsError init function

--- d8/exceptions.py
+++ d8/exceptions.py
@@ -1,4 +1,2 @@
 class InsufficientNumberOfDocumentsError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super(InsufficientNumberOfDocumentsError, self).__init__(message)
+    pass
