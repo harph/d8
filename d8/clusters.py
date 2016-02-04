a39efb064e05741649ba56a2e681187e9e0bff9b
Harrington Joseph <harph@hjoseph.com>
2016-02-03 23:07:09 -0500

    Document class and cluster len unittest

--- d8/clusters.py
+++ d8/clusters.py
@@ -1,10 +1,23 @@
-from exceptions import InsufficientNumberOfDocumentError
+import random
+
+from exceptions import InsufficientNumberOfDocumentsError
+
+
+class Document(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __unicode__(self):
+        return self.text
 
 
 class DocumentCluster(object):
-    def __init__(self, centroid_terms):
-        self.centroid_terms = centroid_terms
-        self.documents = []
+    def __init__(self, initial_doc):
+        self.documents = {initial_doc}
+        self.centroid_terms = {}
+
+    def __unicode__(self):
+        return self.name
 
     @property
     def name(self):
@@ -18,8 +31,12 @@ def generate_document_clusters(documents, num_clusters):
     if len(documents) < num_clusters:
         msg = ('Insufficient number of documents ({docs_len}) for '
                'the amount of clusters requested ({num_clusters})')
-        raise InsufficientNumberOfDocumentError(
+        raise InsufficientNumberOfDocumentsError(
             msg.format(docs_len=len(documents), num_clusters=num_clusters))
 
-    clusters = []
+    clusters = [
+        DocumentCluster(doc)
+        for doc in random.sample(documents, num_clusters)
+    ]
+
     return clusters
--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,25 +1,47 @@
 import unittest
 
-from d8.clusters import generate_document_clusters
+from d8.clusters import Document, generate_document_clusters
 from d8.exceptions import InsufficientNumberOfDocumentsError
 
 
 class TestGenerateCluster(unittest.TestCase):
     def setUp(self):
         # TODO create pseudo-documents
-        self.documents = []
+        self.documents = [
+            Document('Lorem ipsum dolor sit amet.'),
+            Document('Consectetur adipiscing elit.'),
+            Document('Duis scelerisque iaculis libero.'),
+            Document('Duis eleifend pretium tristique.'),
+            Document('Ut porttitor sem justo, id pharetra ipsum maximus eu.'),
+        ]
 
     def test_clusters_len(self):
         """
         Tests that the number of clusters returned matches the requested
         number.
         """
+        documents = self.documents
         # Number of clusters to be generated
-        num_clusters = 5
-        doc_clusters = generate_document_clusters(self.documents, num_clusters)
-        self.assertEquals(len(doc_clusters), num_clusters,
-                         'Number of clusters returned does not match the '
-                         'number of clusters requested.')
+        # Case 1: same number of clusters as documents (one doc per cluster)
+        # Case 2: number of clusters is half the number of documents
+        # Case 3: multiple documents, zero clusters
+        # Case 4: no documents, zero clusters
+        test_cases = (
+            (documents, len(documents)),  # Case 1
+            (documents, len(documents) / 2),  # Case 2
+            (documents, 0),  # Case 3
+            ([], 0),  # Case 4
+        )
+
+        for docs, n_clusters in test_cases:
+            doc_clusters = generate_document_clusters(docs, n_clusters)
+            error_msg = ('Number of clusters returned ({returned}) does not '
+                         'match the number of clusters '
+                         'requested ({requested}).').format(
+                            returned=len(doc_clusters),
+                            requested=n_clusters
+                         )
+            self.assertEquals(len(doc_clusters), n_clusters, error_msg)
 
     def test_insuficient_number_of_documents(self):
         """
